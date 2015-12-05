76e07e802d1a0f25569143925981c605337a1ccf
Trungtr1 <quangtrung.phl@gmail.com>
2015-12-04 16:32:57 -0800

    fix introduce

--- resources/views/introduce.blade.php
+++ resources/views/introduce.blade.php
@@ -17,19 +17,25 @@
 						<br>
 						<div class="row" style="text-align: justify;">
 								<h3 style="color:#428BCA">Giới thiệu</h3>
-								TronDe.vn là phần mềm quản lý ngân hàng câu hỏi và trộn đề online theo yêu cầu. TronDe.vn giúp cho các thầy/cô có thể tổ chức ngân hàng câu hỏi của mình một cách thuận tiện với các tính năng chính như sau<br>
-									-	Ngân hàng câu hỏi được tổ chức theo cấu trúc cây thư mục, dễ quản lý<br>
-									-	Không giới hạn số lượng câu hỏi<br>
-									-	Chia sẻ câu hỏi với nhóm đồng nghiệp hoặc chia sẻ công khai<br>
-									-	Upload/download câu hỏi dạng file word <br>
-									-	Trộn đề từ nhiều file/thư mục khác nhau với số lượng đề, câu không giới hạn.<br>
-									-	Tùy biến mẫu đề in hoặc xuất ra file MS Word.<br>
+								TronDe.vn là phần mềm quản lý ngân hàng câu hỏi và trộn đề online theo yêu cầu. TronDe.vn giúp cho các thầy/cô có thể tổ chức ngân hàng câu hỏi của mình một cách thuận tiện với các tính năng chính như sau
+								<ul>	
+									<li>	Ngân hàng câu hỏi được tổ chức theo cấu trúc cây thư mục, dễ quản lý</li>
+									<li>	Không giới hạn số lượng câu hỏi</li>
+									<li>	Chia sẻ câu hỏi với nhóm đồng nghiệp hoặc chia sẻ công khai</li>
+									<li>	Upload/download câu hỏi dạng file word </li>
+									<li>	Trộn đề từ nhiều file/thư mục khác nhau với số lượng đề, câu không giới hạn.</li>
+									<li>	Tùy biến mẫu đề in hoặc xuất ra file MS Word.</li>
+								</ul>
 								<h3 style="color:#428BCA">Chính sách riêng tư</h3>
-								•	Ngân hàng câu hỏi của các cá nhân được đảm bảo không chia sẻ cho bất kỳ một cá nhân hay tổ chức nào khác.<br>
-								•	Các cá nhân có thể chia sẻ các câu hỏi của mình theo nhóm hoặc công khai.
+								<ul style="padding-left:20px">
+									<li>Ngân hàng câu hỏi của các cá nhân được đảm bảo không chia sẻ cho bất kỳ một cá nhân hay tổ chức nào khác.</li>
+									<li>Các cá nhân có thể chia sẻ các câu hỏi của mình theo nhóm hoặc công khai.</li>
+								</ul>
 								<h3 style="color:#428BCA">Cam kết của TD</h3>
-								-	Miễn phí: www.tronde.vn sẽ luôn miễn phí.<br>
-								-	An toàn: Ngân hàng câu hỏi được bảo mật và không chia sẻ với bất kỳ cá nhân hay tổ chức nào nếu không được sự đồng ý của thầy/cô.
+								<ul style="padding-left:20px">
+									<li>Miễn phí: www.tronde.vn sẽ luôn miễn phí.<br>
+									<li>An toàn: Ngân hàng câu hỏi được bảo mật và không chia sẻ với bất kỳ cá nhân hay tổ chức nào nếu không được sự đồng ý của thầy/cô.
+								</ul>
 						</div>
 					</div>
 				</div>
