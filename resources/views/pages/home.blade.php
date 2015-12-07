d51ccabbfcc9d1ee3991a1fedbce1e9b4d024e57
Trungtr1 <quangtrung.phl@gmail.com>
2015-12-06 22:44:28 -0800

    fix view home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,31 +4,44 @@
 
 @section('content')
 
-<link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="{{ asset('public/css/font-lato.css') }}">
 
-<style>
-	.layout {
-		margin: 150px 0px 0px 0px;
-		width: 100%;
-		display: table;
-		font-weight: 100;
-		font-family: 'Lato';
-		text-align:center;
-	}
-	
-	.title {
-		font-size: 96px;
-	}
-	.title2 {
-		font-size: 48px;
-	}
-</style>
-	<div class="container">
-		<div class="content">
-			<div class="layout">
-				<span class="title">tronde</span><span class="title2">.</span><span class="title">vn</span>
+<div class="container">
+	<div class="content">
+		<div class="row">
+			<div class="col-lg-12 col-md-12">
+				<div class="col-lg-12 col-md-12">
+					<br>
+					<div class="row" style="text-align: justify;">
+							<h3 style="color:#428BCA">Giới thiệu</h3>
+							TronDe.vn là phần mềm quản lý ngân hàng câu hỏi và trộn đề online theo yêu cầu. TronDe.vn giúp cho các thầy/cô có thể tổ chức ngân hàng câu hỏi của mình một cách thuận tiện với các tính năng chính như sau
+							<ul>	
+								<li>	Ngân hàng câu hỏi được tổ chức theo cấu trúc cây thư mục, dễ quản lý</li>
+								<li>	Không giới hạn số lượng câu hỏi</li>
+								<li>	Chia sẻ câu hỏi với nhóm đồng nghiệp hoặc chia sẻ công khai</li>
+								<li>	Upload/download câu hỏi dạng file word </li>
+								<li>	Trộn đề từ nhiều file/thư mục khác nhau với số lượng đề, câu không giới hạn.</li>
+								<li>	Tùy biến mẫu đề in hoặc xuất ra file MS Word.</li>
+							</ul>
+							<h3 style="color:#428BCA">Chính sách riêng tư</h3>
+							<ul style="padding-left:20px">
+								<li>Ngân hàng câu hỏi của các cá nhân được đảm bảo không chia sẻ cho bất kỳ một cá nhân hay tổ chức nào khác.</li>
+								<li>Các cá nhân có thể chia sẻ các câu hỏi của mình theo nhóm hoặc công khai.</li>
+							</ul>
+							<h3 style="color:#428BCA">Cam kết của TD</h3>
+							<ul style="padding-left:20px">
+								<li>Miễn phí: www.tronde.vn sẽ luôn miễn phí.<br>
+								<li>An toàn: Ngân hàng câu hỏi được bảo mật và không chia sẻ với bất kỳ cá nhân hay tổ chức nào nếu không được sự đồng ý của thầy/cô.
+							</ul>
+							<h3 style="color:#428BCA">Đối tượng sử dụng</h3>
+							<ul style="padding-left:20px">
+								<li>Tất cả các thầy, cô có nhu cầu quản lý và tạo đề thi trắc nghiệm cho tất cả các môn học và các cấp học.</li>
+							</ul>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
-</div>	
+</div>
+
 @endsection
